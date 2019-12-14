66ec88ffaf9f9c58512989ef01fd87d6e85ccbc7
Luccas Galuppo Tanan <luccasg.tanan@gmail.com>
2019-12-14 11:24:59 -0300

    Success box

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:alcohol_or_gasoline_flutter/widgets/input.widget.dart';
+import 'package:alcohol_or_gasoline_flutter/widgets/loading-button.widget.dart';
 import 'package:alcohol_or_gasoline_flutter/widgets/logo.widget.dart';
+import 'package:alcohol_or_gasoline_flutter/widgets/success.widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
 
@@ -31,6 +33,10 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           Logo(),
+          Success(
+            reset: () {},
+            result: "Compensa utilizar X",
+          ),
           Input(
             ctrl: _gasCtrl,
             label: "Gasolina",
@@ -39,24 +45,11 @@ class HomePage extends StatelessWidget {
             ctrl: _alcCtrl,
             label: "Álcool",
           ),
-          Container(
-            margin: EdgeInsets.all(30),
-            height: 60,
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(60),
-            ),
-            child: FlatButton(
-              child: Text(
-                "CALCULAR",
-                style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 25,
-                  fontFamily: "Big Shoulders Display"
-                ),
-              ),
-              onPressed: () {},
-            ),
+          LoadingButton(
+            busy: true,
+            func: () {},
+            text: "CALCULAR",
+            invert: false,
           ),
         ],
       ),
